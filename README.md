6da97ab146fc4e109e1acd997da5565d1a754ebd
Zack Fleischman <zFleischman@gmail.com>
2016-04-05 14:37:10 -0700

    Adding drawing and readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# Chord Progressions
+
+Starting from this graph of possible major key chord progressions:
+![alt tag](https://github.com/zFleischman/ChordProgressions/blob/master/ChordGraph.png)
+
+This program generates all the possible permutations of acoustically aesthetic chord progressions.
+
+####Output:
+-I, vii
+-I, ii
+-I, iii
+-I, vi
+-I, IV
+-I, V
+-I, iii, V
+-I, iii, vi
+-I, vi, IV
+-I, iii, vii
+-I, vi, ii
+-I, ii, vii
+-I, vi, V
+-I, iii, IV
+-I, vi, vii
+-I, IV, V
+-I, IV, vii
+-I, iii, ii
+-I, ii, V
+-I, iii, vi, IV
+-I, iii, IV, vii
+-I, vi, ii, vii
+-I, vi, IV, vii
+-I, iii, IV, V
+-I, iii, ii, V
+-I, iii, vi, vii
+-I, iii, vi, V
+-I, vi, IV, V
+-I, vi, ii, V
+-I, iii, vi, ii
+-I, iii, ii, vii
+-I, iii, vi, IV, V
+-I, iii, vi, ii, V
+-I, iii, vi, ii, vii
+-I, iii, vi, IV, vii
--- chords.py
+++ chords.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
 
 final_chords = []
 
 def build_chord_list(G, root, chord_list):
+    """Recursively build up the list of possible chord progressions"""
     global final_chords
     neighbors = G.neighbors(root)
     if not neighbors:
@@ -34,6 +37,18 @@ def print_progression(chord_progression):
     output += chord_progression[-1]
     print(output)
 
+def draw_graph(G):
+    pos = nx.circular_layout(G)
+    pos["I"] = np.array([0,1])
+    pos["iii"] = np.array([0,0.6])
+    pos["vi"] = np.array([0,0.2])
+    pos["ii"] = np.array([-0.2,-0.2])
+    pos["IV"] = np.array([0.2,-0.2])
+    pos["V"] = np.array([-0.2,-0.6])
+    pos["vii"] = np.array([0.2,-0.6])
+    nx.draw(G, pos=pos, with_labels=True)
+    plt.show()
+
 def main(args):
     global final_chords
 
@@ -48,14 +63,15 @@ def main(args):
         ("IV", "vii"),
         ("IV", "V")])
 
+    # Display the graph to verify it's correct.
+    draw_graph(G)
+
     chord_list = ["I"]
     build_chord_list(G, "I", chord_list)
     remove_duplicate_chords()
     sort_by_length()
-
     for c in final_chords:
         print_progression(c)
-
     return 0
 
 if __name__ == '__main__':
